5bed3a18c453ae7dc94587e41ec048295d81298d
LudvigThunberg <ludvigthunberg@gmail.com>
2022-10-10 11:24:29 +0200

    migrated inputs to separated components

--- components/User.tsx
+++ components/User.tsx
@@ -9,9 +9,9 @@ export const User = ({ user }: IUser) => {
   return (
     <div className={styles.container}>
       <h3>
-        Name: {user.firstName} {user.lastName}
+        Name: {user.firstName.value} {user.lastName.value}
       </h3>
-      <p> E-mail: {user.email}</p>
+      <p> E-mail: {user.email.value}</p>
     </div>
   );
 };
