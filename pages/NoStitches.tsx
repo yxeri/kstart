64c95e596938ec108f8f67afa2f15d55a9b35409
LudvigThunberg <ludvigthunberg@gmail.com>
2022-10-12 10:22:16 +0200

    added files

---
+++ pages/NoStitches.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import styles from "../styles/NoStitches.module.css";
+const NoStitches: NextPage = () => {
+  return <div className={styles.pageContainer}></div>;
+};
+
+export default NoStitches;
