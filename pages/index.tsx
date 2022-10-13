f55ad41810175a5615ee554d314ecaa870598dc1
LudvigThunberg <ludvigthunberg@gmail.com>
2022-10-13 13:41:22 +0200

    add Navbar desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Navbar />
       <div className={styles.grid}>
         <Link href="/forms">
           <div className={styles.card}>
