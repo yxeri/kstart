716f286f20dad24305419f1153eb1df5912e6ad2
LudvigThunberg <ludvigthunberg@gmail.com>
2022-10-13 13:43:48 +0200

    deleted cardLinks from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,6 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Navbar />
-      <div className={styles.grid}>
-        <Link href="/forms">
-          <div className={styles.card}>
-            <h2>Forms</h2>
-          </div>
-        </Link>
-      </div>
     </div>
   );
 };
