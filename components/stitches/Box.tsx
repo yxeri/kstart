da95d406b0a660ad4d9fc7b9f4d8866fb90edd40
LudvigThunberg <ludvigthunberg@gmail.com>
2022-10-13 13:48:17 +0200

    added box from branch feature/stitches

---
+++ components/stitches/Box.tsx
@@ -0,0 +1,18 @@
+import { styled } from "../../styles/stitches.config";
+
+export const Box = styled("div", {
+  variants: {
+    variant: {
+      textBox: {
+        minWidth: "300px",
+        maxWidth: "320px",
+        padding: "20px",
+        borderRadius: "20px",
+        border: "2px solid gray",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      },
+    },
+  },
+});
